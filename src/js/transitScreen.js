bd7fed126a059c89d4acddb2632b6d2b26182d6f
msteinhardt <m.steinhardt@gmail.com>
2015-06-25 07:46:28 +0900

    Update transitScreen.js
    Added region parameter to url, to get this to work with Japan.

--- src/js/transitScreen.js
+++ src/js/transitScreen.js
@@ -25,7 +25,7 @@ module.exports = function(journey) {
   var from = encodeURIComponent(journey.from + ',' + journey.location);
   var to = encodeURIComponent(journey.to + ',' + journey.location);
   var url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+from+'&destination='+to+
-  '&mode=transit&departure_time=now&alternatives=true&language=en-gb'; // alternatives for a list; language for time formatting
+  '&mode=transit&departure_time=now&alternatives=true&region=jp&language=en-gb'; // alternatives for a list; language for time formatting
   console.log(url);
   ajax({ 'url': url, 'type': 'json' },
     function(data) {
